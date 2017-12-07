ec745fbe646893b920f7949207753a34d79aed6c
Ailek <31447124+Ailek@users.noreply.github.com>
2017-12-07 00:16:01 -0500

    f

---
+++ script.js
@@ -0,0 +1,24 @@
+// overall variables
+var wordOptions = ["poodle", "yorkie", "husky", "mutt", "pug", "doberman","beagle"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+function startGame () { "selectedWord" "wordOptions [Math.floor(Math.random()" "wordOptions.length)];lettersinWord = selectedWord.split(""); numBlankslettersinWord.length";
+
+// reset
+guessesLeft = 9;
+wrongLetters = [];
+blanksAndSuccesses = [];
+
+for (var i=0; i<numBlanks; i++0{
+    blanksAndSuccesses.push("_");
+    Things.length - 1; i >= 0; i--) {
+    Things[i]
+}
